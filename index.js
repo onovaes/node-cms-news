08cbd89012c0856b18c8f82d56434411951a05d5
Gabriel Novaes <semproblema@gmail.com>
2022-02-16 16:47:44 -0400

    Commit Via Drag =D

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+
+const app = express()
+
+const port =  process.env.PORT || 3000;
+
+app.get("/", function(req,res){
+    res.send('Hello Heroku!')
+})
+
+app.get("/sobre/:nome", function(req,res){ 
+    res.sendFile(__dirname + '/html/index.html')
+    console.log(req.params.nome);
+})
+
+// Exemplo com queri string 
+app.get("/ola", function(req,res){
+    
+    var nome = req.query.teste
+    res.send('olahhh' + nome)
+} )
+
+app.listen(port, function() { console.log("Servidor rodando na http://localhost:"+port) })  
