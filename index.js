86671c9ba4fcac6915fea384d1d8b721548a5294
Gabriel Novaes <semproblema@gmail.com>
2022-02-16 16:55:39 -0400

    heroku tio cehgou

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port =  process.env.PORT || 3000;
 
 app.get("/", function(req,res){
-    res.send('Hello Heroku!')
+    res.send('Hello Heroku! Tio Chegou')
 })
 
 app.get("/sobre/:nome", function(req,res){ 
